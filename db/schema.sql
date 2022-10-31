6bf8d57f93813aafc3f71810da626ecafc5e620f
TiffyAlva <TiffanyAlvarez54@gmail.com>
2022-10-30 21:13:47 -0400

    first commit

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employee_db1;
+CREATE DATABASE employee_db1;
+USE employee_db1;
+
+CREATE TABLE departments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR (100)
+);
+
+CREATE TABLE roles(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100),
+    salary DECIMAL,
+    deptId INTEGER,
+    
+);
+
+CREATE TABLE employees(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(30),
+    lastName VARCHAR(30),
+    roleId INTEGER,
+    managerId INTEGER,
+   
+);
